15d85611159677a577038bee8f42b86a1d674f18
Christian Elies <chris.elies13@gmail.com>
2019-12-03 21:33:14 +0100

    chore(): added code documentation to fetch assets property wrapper

--- Sources/Media/public/PropertyWrappers/FetchAssets.swift
+++ Sources/Media/public/PropertyWrappers/FetchAssets.swift
@@ -7,6 +7,9 @@
 
 import Photos
 
+/// Generic property wrapper for fetching assets from the photo library
+/// Fetches the assets lazily (after accessing the property)
+///
 @propertyWrapper
 public final class FetchAssets<T: MediaProtocol> {
     private let mediaTypePredicate = NSPredicate(format: "mediaType = %d", T.type.rawValue)
@@ -19,6 +22,9 @@ public final class FetchAssets<T: MediaProtocol> {
 
     public var wrappedValue: [T] { assets }
 
+    /// Initializes the property wrapper using a default sort descriptor
+    /// (sort by `creationDate descending`)
+    ///
     public init() {
         let options = PHFetchOptions()
         options.predicate = mediaTypePredicate
@@ -26,6 +32,13 @@ public final class FetchAssets<T: MediaProtocol> {
         self.options = options
     }
 
+    /// Initializes the property wrapper using the given predicate and sort descriptors
+    /// to define the `PHFetchOptions`
+    ///
+    /// - Parameters:
+    ///   - predicate: a predicate for filtering the assets
+    ///   - sortDescriptors: descriptors for sorting the results
+    ///
     public init(predicate: NSPredicate? = nil, sortDescriptors: [NSSortDescriptor]? = nil) {
         let options = PHFetchOptions()
 
