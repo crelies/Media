cd94608c242fcec6033bb5462d10a9a71c2e1d5b
Christian Elies <chris.elies13@gmail.com>
2019-12-05 00:22:27 +0100

    chore(): added code documentation

--- Sources/Media/public/Protocols/MediaSubtypeProvider.swift
+++ Sources/Media/public/Protocols/MediaSubtypeProvider.swift
@@ -5,6 +5,9 @@
 //  Created by Christian Elies on 04.12.19.
 //
 
+/// Requirements for types
+/// representing media subtypes
+///
 public protocol MediaSubtypeProvider {
     var mediaSubtype: MediaSubtype? { get }
 }
