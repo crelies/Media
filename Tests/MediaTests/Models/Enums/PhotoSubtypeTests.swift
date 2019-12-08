c569e8655a5c8d9d29daba7480f99ddc7c5dcdd5
Christian Elies <chris.elies13@gmail.com>
2019-12-08 13:44:35 +0100

    test(): implemented photo subtype tests, created more test files in preparation

---
+++ Tests/MediaTests/Models/Enums/PhotoSubtypeTests.swift
@@ -0,0 +1,36 @@
+//
+//  PhotoSubtypeTests.swift
+//  MediaTests
+//
+//  Created by Christian Elies on 08.12.19.
+//
+
+@testable import Media
+import XCTest
+
+final class PhotoSubtypeTests: XCTestCase {
+    func testPanoramaMediaSubtype() {
+        let subtype: PhotoSubtype = .panorama
+        let expectedMediaSubtype: MediaSubtype = .photoPanorama
+        XCTAssertEqual(subtype.mediaSubtype, expectedMediaSubtype)
+    }
+
+    func testHDRMediaSubtype() {
+        let subtype: PhotoSubtype = .hdr
+        let expectedMediaSubtype: MediaSubtype = .photoHDR
+        XCTAssertEqual(subtype.mediaSubtype, expectedMediaSubtype)
+    }
+
+    func testScreenshotMediaSubtype() {
+        let subtype: PhotoSubtype = .screenshot
+        let expectedMediaSubtype: MediaSubtype = .photoScreenshot
+        XCTAssertEqual(subtype.mediaSubtype, expectedMediaSubtype)
+    }
+
+    @available(iOS 10.2, *)
+    func testDepthEffectMediaSubtype() {
+        let subtype: PhotoSubtype = .depthEffect
+        let expectedMediaSubtype: MediaSubtype = .photoDepthEffect
+        XCTAssertEqual(subtype.mediaSubtype, expectedMediaSubtype)
+    }
+}
