389bda3c7e3f90acd710022c67f0d5d55c18a5a2
Christian Elies <chris.elies13@gmail.com>
2019-12-08 20:32:54 +0100

    test(): ui image picker controller + media type tests

--- Sources/Media/private/Extensions/UIImagePickerController/UIImagePickerController+MediaType.swift
+++ Sources/Media/private/Extensions/UIImagePickerController/UIImagePickerController+MediaType.swift
@@ -14,29 +14,31 @@ extension UIImagePickerController {
         case image
         case livePhoto
         case movie
+    }
+}
 
-        var cfString: CFString {
-            switch self {
-            case .image:
-                return CFString.image
-            case .livePhoto:
-                return CFString.livePhoto
-            case .movie:
-                return CFString.movie
-            }
+extension UIImagePickerController.MediaType {
+    var cfString: CFString {
+        switch self {
+        case .image:
+            return CFString.image
+        case .livePhoto:
+            return CFString.livePhoto
+        case .movie:
+            return CFString.movie
         }
+    }
 
-        init(string: String) throws {
-            switch string as CFString {
-            case CFString.image:
-                self = UIImagePickerController.MediaType.image
-            case CFString.livePhoto:
-                self = UIImagePickerController.MediaType.livePhoto
-            case CFString.movie:
-                self = UIImagePickerController.MediaType.movie
-            default:
-                throw UIImagePickerController.MediaTypeError.unsupportedString
-            }
+    init(string: String) throws {
+        switch string as CFString {
+        case CFString.image:
+            self = UIImagePickerController.MediaType.image
+        case CFString.livePhoto:
+            self = UIImagePickerController.MediaType.livePhoto
+        case CFString.movie:
+            self = UIImagePickerController.MediaType.movie
+        default:
+            throw UIImagePickerController.MediaTypeError.unsupportedString
         }
     }
 }
---
+++ Tests/MediaTests/Extensions/UIImagePickerController+MediaTypeTests.swift
@@ -0,0 +1,45 @@
+//
+//  UIImagePickerController+MediaTypeTests.swift
+//  MediaTests
+//
+//  Created by Christian Elies on 08.12.19.
+//
+
+@testable import Media
+import MobileCoreServices
+import XCTest
+
+final class UIImagePickerController_MediaTypeTests: XCTestCase {
+    func testImageCFString() {
+        let mediaType: UIImagePickerController.MediaType = .image
+        XCTAssertEqual(mediaType.cfString, .image)
+    }
+
+    func testLivePhotoCFString() {
+        let mediaType: UIImagePickerController.MediaType = .livePhoto
+        XCTAssertEqual(mediaType.cfString, .livePhoto)
+    }
+
+    func testMovieCFString() {
+        let mediaType: UIImagePickerController.MediaType = .movie
+        XCTAssertEqual(mediaType.cfString, .movie)
+    }
+
+    func testSuccessfulInit() {
+        do {
+            _ = try UIImagePickerController.MediaType(string: kUTTypeMovie as String)
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+    }
+
+    func testErrorOnInit() {
+        do {
+            _ = try UIImagePickerController.MediaType(string: "aaa")
+            XCTFail("Successful init is unexpected")
+        } catch {
+            let error = error as? UIImagePickerController.MediaTypeError
+            XCTAssertEqual(error, .unsupportedString)
+        }
+    }
+}
