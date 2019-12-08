d67b8f2603276bc97b1f91295388f66459d655f6
Christian Elies <chris.elies13@gmail.com>
2019-12-08 15:30:50 +0100

    test(): implemented media swiftui tests, implemented ph asset changer favorite tests

---
+++ Tests/MediaTests/Models/Media/MediaSwiftUITests.swift
@@ -0,0 +1,22 @@
+//
+//  MediaSwiftUITests.swift
+//  MediaTests
+//
+//  Created by Christian Elies on 08.12.19.
+//
+
+@testable import Media
+import Photos
+import XCTest
+
+@available(iOS 13, *)
+final class MediaSwiftUITests: XCTestCase {
+    func testBrowser() {
+        do {
+            let completion: (Result<PHAsset, Error>) -> Void = { result in }
+            _ = try Media.browser(completion)
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+    }
+}
--- Tests/MediaTests/Services/PHAssetChangerTests.swift
+++ Tests/MediaTests/Services/PHAssetChangerTests.swift
@@ -6,19 +6,92 @@
 //
 
 @testable import Media
+import Photos
 import XCTest
 
 @available(macOS 10.15, *)
 final class PHAssetChangerTests: XCTestCase {
     override func setUp() {
+        Media.photoLibrary = MockPhotoLibrary.self
         PHAssetChanger.photoLibrary = MockPhotoLibrary()
+
+        MockPhotoLibrary.authorizationStatusToReturn = .authorized
+        MockPhotoLibrary.performChangesSuccess = true
+        MockPhotoLibrary.performChangesError = nil
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        
+    func testFavoriteWithoutPermission() {
+        MockPhotoLibrary.authorizationStatusToReturn = .denied
+
+        let expectation = self.expectation(description: "RequestCompleted")
+
+        var result: Result<Void, Error>?
+        PHAssetChanger.favorite(phAsset: PHAsset(), favorite: false) { res in
+            result = res
+            expectation.fulfill()
+        }
+
+        waitForExpectations(timeout: 2)
+
+        XCTAssertNotNil(result)
+
+        switch result {
+        case .failure(let error):
+            let permissionError = error as? PermissionError
+            XCTAssertEqual(permissionError, PermissionError.denied)
+        default:
+            XCTFail()
+        }
+    }
+
+    func testFavoriteWithError() {
+        MockPhotoLibrary.performChangesSuccess = false
+        MockPhotoLibrary.performChangesError = MediaError.unknown
+
+        let expectation = self.expectation(description: "RequestCompleted")
+
+        var result: Result<Void, Error>?
+        PHAssetChanger.favorite(phAsset: PHAsset(), favorite: false) { res in
+            result = res
+            expectation.fulfill()
+        }
+
+        waitForExpectations(timeout: 2)
+
+        XCTAssertNotNil(result)
+
+        switch result {
+        case .failure(let error):
+            let mediaError = error as? MediaError
+            XCTAssertEqual(mediaError, MediaError.unknown)
+        default:
+            XCTFail()
+        }
+    }
+
+    func testFavoriteWithSuccess() {
+        let expectation = self.expectation(description: "RequestCompleted")
+
+        var result: Result<Void, Error>?
+        PHAssetChanger.favorite(phAsset: PHAsset(), favorite: false) { res in
+            result = res
+            expectation.fulfill()
+        }
+
+        waitForExpectations(timeout: 2)
+
+        XCTAssertNotNil(result)
+
+        switch result {
+        case .success: ()
+        case .failure(let error):
+            XCTFail(error.localizedDescription)
+        case .none:
+            XCTFail()
+        }
     }
 }
