b86d778b998a3a0e757e9bda6668bcee0edce75f
Christian Elies <chris.elies13@gmail.com>
2019-12-08 15:19:02 +0100

    test(): implemented video export quality tests

--- Tests/MediaTests/Models/Video/VideoExportQualityTests.swift
+++ Tests/MediaTests/Models/Video/VideoExportQualityTests.swift
@@ -5,19 +5,56 @@
 //  Created by Christian Elies on 08.12.19.
 //
 
+import AVFoundation
+@testable import Media
 import XCTest
 
 final class VideoExportQualityTests: XCTestCase {
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testLowAVAssetExportPreset() {
+        let quality: Video.ExportQuality = .low
+        let expectedExportPreset = AVAssetExportPresetLowQuality
+        XCTAssertEqual(quality.avAssetExportPreset, expectedExportPreset)
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testMediumAVAssetExportPreset() {
+        let quality: Video.ExportQuality = .medium
+        let expectedExportPreset = AVAssetExportPresetMediumQuality
+        XCTAssertEqual(quality.avAssetExportPreset, expectedExportPreset)
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testHighestAVAssetExportPreset() {
+        let quality: Video.ExportQuality = .highest
+        let expectedExportPreset = AVAssetExportPresetHighestQuality
+        XCTAssertEqual(quality.avAssetExportPreset, expectedExportPreset)
+    }
+
+    func testq640x480AVAssetExportPreset() {
+        let quality: Video.ExportQuality = .q640x480
+        let expectedExportPreset = AVAssetExportPreset640x480
+        XCTAssertEqual(quality.avAssetExportPreset, expectedExportPreset)
+    }
+
+    func testq960x540AVAssetExportPreset() {
+        let quality: Video.ExportQuality = .q960x540
+        let expectedExportPreset = AVAssetExportPreset960x540
+        XCTAssertEqual(quality.avAssetExportPreset, expectedExportPreset)
+    }
+
+    func testq1280x720AVAssetExportPreset() {
+        let quality: Video.ExportQuality = .q1280x720
+        let expectedExportPreset = AVAssetExportPreset1280x720
+        XCTAssertEqual(quality.avAssetExportPreset, expectedExportPreset)
+    }
+
+    func testq1920x1080AVAssetExportPreset() {
+        let quality: Video.ExportQuality = .q1920x1080
+        let expectedExportPreset = AVAssetExportPreset1920x1080
+        XCTAssertEqual(quality.avAssetExportPreset, expectedExportPreset)
+    }
+
+    func testq3840x2160AVAssetExportPreset() {
+        let quality: Video.ExportQuality = .q3840x2160
+        let expectedExportPreset = AVAssetExportPreset3840x2160
+        XCTAssertEqual(quality.avAssetExportPreset, expectedExportPreset)
     }
 }
