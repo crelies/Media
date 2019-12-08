cde52aa772b68bdf4818a14b0b0e7560beb5094e
Christian Elies <chris.elies13@gmail.com>
2019-12-08 20:55:50 +0100

    test(): implemented ph authorization status + permission error tests

---
+++ Tests/MediaTests/Extensions/PHAuthorizationStatus+PermissionErrorTests.swift
@@ -0,0 +1,32 @@
+//
+//  PHAuthorizationStatus+PermissionErrorTests.swift
+//  MediaTests
+//
+//  Created by Christian Elies on 08.12.19.
+//
+
+@testable import Media
+import Photos
+import XCTest
+
+final class PHAuthorizationStatus_PermissionErrorTests: XCTestCase {
+    func testDenied() {
+        let authorizationStatus: PHAuthorizationStatus = .denied
+        XCTAssertEqual(authorizationStatus.permissionError, .denied)
+    }
+
+    func testNotDetermined() {
+        let authorizationStatus: PHAuthorizationStatus = .notDetermined
+        XCTAssertEqual(authorizationStatus.permissionError, .notDetermined)
+    }
+
+    func testRestricted() {
+        let authorizationStatus: PHAuthorizationStatus = .restricted
+        XCTAssertEqual(authorizationStatus.permissionError, .restricted)
+    }
+
+    func testAuthorized() {
+        let authorizationStatus: PHAuthorizationStatus = .authorized
+        XCTAssertNil(authorizationStatus.permissionError)
+    }
+}
