aadb9bc33599c4db0a62405e7a9c5ce362afba5f
Christian Elies <chris.elies13@gmail.com>
2019-12-08 21:07:53 +0100

    test(): implemented timer wrapper tests

---
+++ Tests/MediaTests/TimerWrapperTests.swift
@@ -0,0 +1,27 @@
+//
+//  TimerWrapperTests.swift
+//  MediaTests
+//
+//  Created by Christian Elies on 08.12.19.
+//
+
+@testable import Media
+import XCTest
+
+final class TimerWrapperTests: XCTestCase {
+    func testTimerFired() {
+        let expectation = self.expectation(description: "TimerFired")
+
+        var timerFired = false
+        let timerWrapper = TimerWrapper(timeInterval: 1, repeats: true) { timer in
+            timerFired = true
+            timer.invalidate()
+            expectation.fulfill()
+        }
+        RunLoop.main.add(timerWrapper.timer, forMode: .common)
+
+        waitForExpectations(timeout: 2)
+
+        XCTAssertTrue(timerFired)
+    }
+}
