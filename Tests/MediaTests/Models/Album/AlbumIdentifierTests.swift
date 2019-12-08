74e8317c308742b3f3f012816a6fd2e968af1450
Christian Elies <chris.elies13@gmail.com>
2019-12-08 21:10:48 +0100

    test(): implemented album identifier tests

---
+++ Tests/MediaTests/Models/Album/AlbumIdentifierTests.swift
@@ -0,0 +1,23 @@
+//
+//  AlbumIdentifierTests.swift
+//  MediaTests
+//
+//  Created by Christian Elies on 08.12.19.
+//
+
+@testable import Media
+import XCTest
+
+final class AlbumIdentifierTests: XCTestCase {
+    func testExpressibleByStringLiteral() {
+        let identifier = "Test"
+        let albumIdentifier = Album.Identifier(stringLiteral: identifier)
+        XCTAssertEqual(albumIdentifier.localIdentifier, identifier)
+    }
+
+    func testDescription() {
+        let identifier = "Test"
+        let albumIdentifier = Album.Identifier(stringLiteral: identifier)
+        XCTAssertEqual(albumIdentifier.description, identifier)
+    }
+}
