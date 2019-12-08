883f5047dd1c2755fe8e0b1824f8bc60ecaa5a6e
Christian Elies <chris.elies13@gmail.com>
2019-12-08 15:10:56 +0100

    test(): implemented photo type tests

--- Tests/MediaTests/Models/Photo/PhotoFileTypeTests.swift
+++ Tests/MediaTests/Models/Photo/PhotoFileTypeTests.swift
@@ -5,19 +5,69 @@
 //  Created by Christian Elies on 08.12.19.
 //
 
+import AVFoundation
+@testable import Media
 import XCTest
 
+@available(iOS 11, *)
 final class PhotoFileTypeTests: XCTestCase {
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testAVCIPathExtensions() {
+        let fileType: Photo.FileType = .avci
+        let expectedPathExtensions: [String] = ["avci"]
+        XCTAssertEqual(fileType.pathExtensions, expectedPathExtensions)
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testAVCIAVFileType() {
+        let fileType: Photo.FileType = .avci
+        let expectedAVFileType: AVFileType = .avci
+        XCTAssertEqual(fileType.avFileType, expectedAVFileType)
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testHEICPathExtensions() {
+        let fileType: Photo.FileType = .heic
+        let expectedPathExtensions: [String] = ["heic"]
+        XCTAssertEqual(fileType.pathExtensions, expectedPathExtensions)
+    }
+
+    func testHEICAVFileType() {
+        let fileType: Photo.FileType = .heic
+        let expectedAVFileType: AVFileType = .heic
+        XCTAssertEqual(fileType.avFileType, expectedAVFileType)
+    }
+
+    func testHEIFPathExtensions() {
+        let fileType: Photo.FileType = .heif
+        let expectedPathExtensions: [String] = ["heif"]
+        XCTAssertEqual(fileType.pathExtensions, expectedPathExtensions)
+    }
+
+    func testHEIFAVFileType() {
+        let fileType: Photo.FileType = .heif
+        let expectedAVFileType: AVFileType = .heif
+        XCTAssertEqual(fileType.avFileType, expectedAVFileType)
+    }
+
+    func testJPGPathExtensions() {
+        let fileType: Photo.FileType = .jpg
+        let expectedPathExtensions: [String] = ["jpg", "jpeg"]
+        XCTAssertEqual(fileType.pathExtensions, expectedPathExtensions)
+    }
+
+    func testJPGAVFileType() {
+        let fileType: Photo.FileType = .jpg
+        let expectedAVFileType: AVFileType = .jpg
+        XCTAssertEqual(fileType.avFileType, expectedAVFileType)
+    }
+
+    func testTIFPathExtensions() {
+        let fileType: Photo.FileType = .tif
+        let expectedPathExtensions: [String] = ["tiff", "tif"]
+        XCTAssertEqual(fileType.pathExtensions, expectedPathExtensions)
+    }
+
+    func testTIFAVFileType() {
+        let fileType: Photo.FileType = .tif
+        let expectedAVFileType: AVFileType = .tif
+        XCTAssertEqual(fileType.avFileType, expectedAVFileType)
     }
 }
