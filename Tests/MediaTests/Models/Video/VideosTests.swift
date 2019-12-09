4d8f49e0234a441da2efa40eaf7cc95c055e7294
Christian Elies <chris.elies13@gmail.com>
2019-12-09 21:26:27 +0100

    test(): implemented videos tests

---
+++ Tests/MediaTests/Models/Video/VideosTests.swift
@@ -0,0 +1,67 @@
+//
+//  VideosTests.swift
+//  MediaTests
+//
+//  Created by Christian Elies on 09.12.19.
+//
+
+@testable import Media
+import XCTest
+
+final class VideosTests: XCTestCase {
+    let mockAsset = MockPHAsset()
+
+    override func setUp() {
+        PHAssetFetcher.asset = MockPHAsset.self
+        MockPHAsset.fetchResult.mockAssets.removeAll()
+        mockAsset.mediaTypeToReturn = .video
+        mockAsset.mediaSubtypesToReturn = []
+    }
+
+    func testAllEmpty() {
+        let videos = Videos.all
+        XCTAssertTrue(videos.isEmpty)
+    }
+
+    func testAllNotEmpty() {
+        MockPHAsset.fetchResult.mockAssets = [mockAsset]
+        let videos = Videos.all
+        XCTAssertFalse(videos.isEmpty)
+    }
+
+    func testStreamsEmpty() {
+        let videos = Videos.streams
+        XCTAssertTrue(videos.isEmpty)
+    }
+
+    func testStreamsNotEmpty() {
+        mockAsset.mediaSubtypesToReturn = [.videoStreamed]
+        MockPHAsset.fetchResult.mockAssets = [mockAsset]
+        let videos = Videos.streams
+        XCTAssertFalse(videos.isEmpty)
+    }
+
+    func testHighFrameRatesEmpty() {
+        let videos = Videos.highFrameRates
+        XCTAssertTrue(videos.isEmpty)
+    }
+
+    func testHighFrameRatesNotEmpty() {
+        mockAsset.mediaSubtypesToReturn = [.videoHighFrameRate]
+        MockPHAsset.fetchResult.mockAssets = [mockAsset]
+        let videos = Videos.highFrameRates
+        XCTAssertFalse(videos.isEmpty)
+    }
+
+    func testTimelapsesEmpty() {
+        let videos = Videos.timelapses
+        XCTAssertTrue(videos.isEmpty)
+    }
+
+    func testTimelapsesNotEmpty() {
+        mockAsset.mediaSubtypesToReturn = [.videoTimelapse]
+        MockPHAsset.fetchResult.mockAssets = [mockAsset]
+        let videos = Videos.timelapses
+        XCTAssertFalse(videos.isEmpty)
+    }
+}
