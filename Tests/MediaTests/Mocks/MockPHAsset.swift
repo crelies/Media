ca6a14d338da41cc0c4062fee396cd879d7ca79f
Christian Elies <chris.elies13@gmail.com>
2019-12-09 21:21:17 +0100

    test(): implemented photos tests

--- Tests/MediaTests/Mocks/MockPHAsset.swift
+++ Tests/MediaTests/Mocks/MockPHAsset.swift
@@ -9,6 +9,11 @@ import Photos
 
 final class MockPHAsset: PHAsset {
     static var fetchResult = MockPHAssetsFetchResult()
+    var mediaTypeToReturn: PHAssetMediaType = .image
+    var mediaSubtypesToReturn: PHAssetMediaSubtype = []
+
+    override var mediaType: PHAssetMediaType { mediaTypeToReturn }
+    override var mediaSubtypes: PHAssetMediaSubtype { mediaSubtypesToReturn }
 
     override class func fetchAssets(with options: PHFetchOptions?) -> PHFetchResult<PHAsset> {
         fetchResult
---
+++ Tests/MediaTests/Models/Photo/PhotosTests.swift
@@ -0,0 +1,93 @@
+//
+//  PhotosTests.swift
+//  MediaTests
+//
+//  Created by Christian Elies on 09.12.19.
+//
+
+@testable import Media
+import XCTest
+
+final class PhotosTests: XCTestCase {
+    let mockAsset = MockPHAsset()
+
+    override func setUp() {
+        PHAssetFetcher.asset = MockPHAsset.self
+        MockPHAsset.fetchResult.mockAssets.removeAll()
+        mockAsset.mediaTypeToReturn = .image
+        mockAsset.mediaSubtypesToReturn = []
+    }
+
+    func testAllEmpty() {
+        let photos = Media.Photos.all
+        XCTAssertTrue(photos.isEmpty)
+    }
+
+    func testAllNotEmpty() {
+        MockPHAsset.fetchResult.mockAssets = [mockAsset]
+        let photos = Media.Photos.all
+        XCTAssertFalse(photos.isEmpty)
+    }
+
+    func testLiveEmpty() {
+        let photos = Media.Photos.live
+        XCTAssertTrue(photos.isEmpty)
+    }
+
+    func testLiveNotEmpty() {
+        mockAsset.mediaSubtypesToReturn = [.photoLive]
+        MockPHAsset.fetchResult.mockAssets = [mockAsset]
+        let photos = Media.Photos.live
+        XCTAssertFalse(photos.isEmpty)
+    }
+
+    func testPanoramaEmpty() {
+        let photos = Media.Photos.panorama
+        XCTAssertTrue(photos.isEmpty)
+    }
+
+    func testPanoramaNotEmpty() {
+        mockAsset.mediaSubtypesToReturn = [.photoPanorama]
+        MockPHAsset.fetchResult.mockAssets = [mockAsset]
+        let photos = Media.Photos.panorama
+        XCTAssertFalse(photos.isEmpty)
+    }
+
+    func testHDREmpty() {
+        let photos = Media.Photos.hdr
+        XCTAssertTrue(photos.isEmpty)
+    }
+
+    func testHDRNotEmpty() {
+        mockAsset.mediaSubtypesToReturn = [.photoHDR]
+        MockPHAsset.fetchResult.mockAssets = [mockAsset]
+        let photos = Media.Photos.hdr
+        XCTAssertFalse(photos.isEmpty)
+    }
+
+    func testScreenshotEmpty() {
+        let photos = Media.Photos.screenshot
+        XCTAssertTrue(photos.isEmpty)
+    }
+
+    func testScreenshotNotEmpty() {
+        mockAsset.mediaSubtypesToReturn = [.photoScreenshot]
+        MockPHAsset.fetchResult.mockAssets = [mockAsset]
+        let photos = Media.Photos.screenshot
+        XCTAssertFalse(photos.isEmpty)
+    }
+
+    @available(iOS 10.2, *)
+    func testDepthEffectEmpty() {
+        let photos = Media.Photos.depthEffect
+        XCTAssertTrue(photos.isEmpty)
+    }
+
+    @available(iOS 10.2, *)
+    func testDepthEffectNotEmpty() {
+        mockAsset.mediaSubtypesToReturn = [.photoDepthEffect]
+        MockPHAsset.fetchResult.mockAssets = [mockAsset]
+        let photos = Media.Photos.depthEffect
+        XCTAssertFalse(photos.isEmpty)
+    }
+}
