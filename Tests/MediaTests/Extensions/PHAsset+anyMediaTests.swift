0eb22c914bcf4753224de4caa75baa9e26a87449
Christian Elies <chris.elies13@gmail.com>
2019-12-14 21:45:56 +0100

    test(): implemented ph asset + any media tests

---
+++ Tests/MediaTests/Extensions/PHAsset+anyMediaTests.swift
@@ -0,0 +1,43 @@
+//
+//  PHAsset+anyMediaTests.swift
+//  MediaTests
+//
+//  Created by Christian Elies on 14.12.19.
+//
+
+@testable import Media
+import Photos
+import XCTest
+
+final class PHAsset_anyMediaTests: XCTestCase {
+    func testAudioAnyMedia() {
+        let asset = MockPHAsset()
+        asset.mediaTypeToReturn = .audio
+        XCTAssertNotNil(asset.anyMedia?.value as? Audio)
+    }
+
+    func testLivePhotoAnyMedia() {
+        let asset = MockPHAsset()
+        asset.mediaTypeToReturn = .image
+        asset.mediaSubtypesToReturn = .photoLive
+        XCTAssertNotNil(asset.anyMedia?.value as? LivePhoto)
+    }
+
+    func testPhotoAnyMedia() {
+        let asset = MockPHAsset()
+        asset.mediaTypeToReturn = .image
+        XCTAssertNotNil(asset.anyMedia?.value as? Photo)
+    }
+
+    func testVideoAnyMedia() {
+        let asset = MockPHAsset()
+        asset.mediaTypeToReturn = .video
+        XCTAssertNotNil(asset.anyMedia?.value as? Video)
+    }
+
+    func testUnknownAnyMedia() {
+        let asset = MockPHAsset()
+        asset.mediaTypeToReturn = .unknown
+        XCTAssertNil(asset.anyMedia)
+    }
+}
