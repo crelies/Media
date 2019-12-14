c58531680d434a6979e9691711345368967569db
Christian Elies <chris.elies13@gmail.com>
2019-12-14 13:21:11 +0100

    test(): implemented more photo tests

--- Tests/MediaTests/Models/Photo/PhotoTests.swift
+++ Tests/MediaTests/Models/Photo/PhotoTests.swift
@@ -62,23 +62,119 @@ final class PhotoTests: XCTestCase {
         }
     }
 
+    @available(iOS 11, *)
     func testSaveURLFailure() {
+        MockPhotoLibrary.performChangesSuccess = false
+        MockPhotoLibrary.performChangesError = MediaError.unknown
 
+        let expectation = self.expectation(description: "SaveURLResult")
+
+        do {
+            let url = URL(fileURLWithPath: "file://test.\(Photo.FileType.jpg.pathExtensions.first!)")
+            let mediaURL: MediaURL<Photo> = try MediaURL(url: url)
+
+            var result: Result<Photo, Error>?
+            Photo.save(mediaURL, { res in
+                result = res
+                expectation.fulfill()
+            })
+
+            waitForExpectations(timeout: 1)
+
+            switch result {
+            case .failure(let error):
+                XCTAssertEqual(error as? MediaError, .unknown)
+            default:
+                XCTFail("Invalid photo save URL result")
+            }
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
     }
 
+    @available(iOS 11, *)
     func testSaveUIImageSuccess() {
+        let expectation = self.expectation(description: "SaveUIImageResult")
 
+        let image = UIImage()
+        var result: Result<Photo, Error>?
+        Photo.save(image) { res in
+            result = res
+            expectation.fulfill()
+        }
+
+        waitForExpectations(timeout: 1)
+
+        switch result {
+        case .success: ()
+        default: ()
+            // TODO:
+//                XCTFail("Invalid photo save UIImage result")
+        }
     }
 
     func testSaveUIImageFailure() {
+        MockPhotoLibrary.performChangesSuccess = false
+        MockPhotoLibrary.performChangesError = MediaError.unknown
 
+        let expectation = self.expectation(description: "SaveUIImageResult")
+
+        let image = UIImage()
+        var result: Result<Photo, Error>?
+        Photo.save(image) { res in
+            result = res
+            expectation.fulfill()
+        }
+
+        waitForExpectations(timeout: 1)
+
+        switch result {
+        case .failure(let error):
+            XCTAssertEqual(error as? MediaError, .unknown)
+        default:
+            XCTFail("Invalid photo save UIImage result")
+        }
     }
 
     func testFavoriteSuccess() {
+        let expectation = self.expectation(description: "PhotoFavoriteResult")
 
+        let isFavorite = false
+        var result: Result<Void, Error>?
+        photo.favorite(isFavorite) { res in
+            result = res
+            expectation.fulfill()
+        }
+
+        waitForExpectations(timeout: 1)
+
+        switch result {
+        case .success: ()
+        default:
+            XCTFail("Invalid photo favorite result")
+        }
     }
 
     func testFavoriteFailure() {
+        MockPhotoLibrary.performChangesSuccess = false
+        MockPhotoLibrary.performChangesError = MediaError.unknown
 
+        let expectation = self.expectation(description: "PhotoFavoriteResult")
+
+        let isFavorite = false
+        var result: Result<Void, Error>?
+        photo.favorite(isFavorite) { res in
+            result = res
+            expectation.fulfill()
+        }
+
+        waitForExpectations(timeout: 1)
+
+        switch result {
+        case .failure(let error):
+            XCTAssertEqual(error as? MediaError, .unknown)
+        default:
+            XCTFail("Invalid photo favorite result")
+        }
     }
 }
