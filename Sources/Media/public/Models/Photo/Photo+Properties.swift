e7f39806d05aa51d40a292603f0760c0af43beea
Christian Elies <chris.elies13@gmail.com>
2020-01-21 23:34:57 +0100

    fix(photo): dictionary parsing issues

--- Sources/Media/public/Models/Photo/Photo+Properties.swift
+++ Sources/Media/public/Models/Photo/Photo+Properties.swift
@@ -91,77 +91,80 @@ extension Photo {
 
 extension Photo.Properties {
     init(dictionary: [String: Any]) {
+        let exifDictionary = dictionary["{Exif}"] as? [String: Any]
         var exif = EXIF(
-            apertureValue: dictionary[kCGImagePropertyExifApertureValue as String] as? String,
-            brightnessValue: dictionary[kCGImagePropertyExifBrightnessValue as String] as? String,
-            colorSpace: dictionary[kCGImagePropertyExifColorSpace as String] as? Int,
-            componentsConfiguration: dictionary[kCGImagePropertyExifComponentsConfiguration as String] as? [Int],
-            dateTimeDigitized: dictionary[kCGImagePropertyExifDateTimeDigitized as String] as? String,
-            dateTimeOriginal: dictionary[kCGImagePropertyExifDateTimeOriginal as String] as? String,
-            exifVersion: dictionary[kCGImagePropertyExifVersion as String] as? [Int],
-            exposureBiasValue: dictionary[kCGImagePropertyExifExposureBiasValue as String] as? Int,
-            exposureMode: dictionary[kCGImagePropertyExifExposureMode as String] as? Int,
-            exposureProgram: dictionary[kCGImagePropertyExifExposureProgram as String] as? Int,
-            exposureTime: dictionary[kCGImagePropertyExifExposureTime as String] as? String,
-            fNumber: dictionary[kCGImagePropertyExifFNumber as String] as? String,
-            flash: dictionary[kCGImagePropertyExifFlash as String] as? Int,
-            flashPixVersion: dictionary[kCGImagePropertyExifFlashPixVersion as String] as? [Int],
-            focalLenIn35mmFilm: dictionary[kCGImagePropertyExifFocalLenIn35mmFilm as String] as? Int,
-            focalLength: dictionary[kCGImagePropertyExifFocalLength as String] as? String,
-            isoSpeedRatings: dictionary[kCGImagePropertyExifISOSpeedRatings as String] as? [Int],
-            lensMake: dictionary[kCGImagePropertyExifLensMake as String] as? String,
-            lensModel: dictionary[kCGImagePropertyExifLensModel as String] as? String,
-            lensSpecification: dictionary[kCGImagePropertyExifLensSpecification as String] as? [String],
-            meteringMode: dictionary[kCGImagePropertyExifMeteringMode as String] as? Int,
-            pixelXDimension: dictionary[kCGImagePropertyExifPixelXDimension as String] as? Int,
-            pixelYDimension: dictionary[kCGImagePropertyExifPixelYDimension as String] as? Int,
-            sceneCaptureType: dictionary[kCGImagePropertyExifSceneCaptureType as String] as? Int,
-            sceneType: dictionary[kCGImagePropertyExifSceneType as String] as? Int,
-            sensingMethod: dictionary[kCGImagePropertyExifSensingMethod as String] as? Int,
-            shutterSpeedValue: dictionary[kCGImagePropertyExifShutterSpeedValue as String] as? String,
-            subjectArea: dictionary[kCGImagePropertyExifSubjectArea as String] as? [Int],
-            subsecTimeDigitized: dictionary[kCGImagePropertyExifSubsecTimeDigitized as String] as? Int,
-            subsecTimeOriginal: dictionary[kCGImagePropertyExifSubsecTimeOrginal as String] as? Int,
-            whiteBalance: dictionary[kCGImagePropertyExifWhiteBalance as String] as? Int
+            apertureValue: exifDictionary?[kCGImagePropertyExifApertureValue as String] as? String,
+            brightnessValue: exifDictionary?[kCGImagePropertyExifBrightnessValue as String] as? String,
+            colorSpace: exifDictionary?[kCGImagePropertyExifColorSpace as String] as? Int,
+            componentsConfiguration: exifDictionary?[kCGImagePropertyExifComponentsConfiguration as String] as? [Int],
+            dateTimeDigitized: exifDictionary?[kCGImagePropertyExifDateTimeDigitized as String] as? String,
+            dateTimeOriginal: exifDictionary?[kCGImagePropertyExifDateTimeOriginal as String] as? String,
+            exifVersion: exifDictionary?[kCGImagePropertyExifVersion as String] as? [Int],
+            exposureBiasValue: exifDictionary?[kCGImagePropertyExifExposureBiasValue as String] as? Int,
+            exposureMode: exifDictionary?[kCGImagePropertyExifExposureMode as String] as? Int,
+            exposureProgram: exifDictionary?[kCGImagePropertyExifExposureProgram as String] as? Int,
+            exposureTime: exifDictionary?[kCGImagePropertyExifExposureTime as String] as? String,
+            fNumber: exifDictionary?[kCGImagePropertyExifFNumber as String] as? String,
+            flash: exifDictionary?[kCGImagePropertyExifFlash as String] as? Int,
+            flashPixVersion: exifDictionary?[kCGImagePropertyExifFlashPixVersion as String] as? [Int],
+            focalLenIn35mmFilm: exifDictionary?[kCGImagePropertyExifFocalLenIn35mmFilm as String] as? Int,
+            focalLength: exifDictionary?[kCGImagePropertyExifFocalLength as String] as? String,
+            isoSpeedRatings: exifDictionary?[kCGImagePropertyExifISOSpeedRatings as String] as? [Int],
+            lensMake: exifDictionary?[kCGImagePropertyExifLensMake as String] as? String,
+            lensModel: exifDictionary?[kCGImagePropertyExifLensModel as String] as? String,
+            lensSpecification: exifDictionary?[kCGImagePropertyExifLensSpecification as String] as? [String],
+            meteringMode: exifDictionary?[kCGImagePropertyExifMeteringMode as String] as? Int,
+            pixelXDimension: exifDictionary?[kCGImagePropertyExifPixelXDimension as String] as? Int,
+            pixelYDimension: exifDictionary?[kCGImagePropertyExifPixelYDimension as String] as? Int,
+            sceneCaptureType: exifDictionary?[kCGImagePropertyExifSceneCaptureType as String] as? Int,
+            sceneType: exifDictionary?[kCGImagePropertyExifSceneType as String] as? Int,
+            sensingMethod: exifDictionary?[kCGImagePropertyExifSensingMethod as String] as? Int,
+            shutterSpeedValue: exifDictionary?[kCGImagePropertyExifShutterSpeedValue as String] as? String,
+            subjectArea: exifDictionary?[kCGImagePropertyExifSubjectArea as String] as? [Int],
+            subsecTimeDigitized: exifDictionary?[kCGImagePropertyExifSubsecTimeDigitized as String] as? Int,
+            subsecTimeOriginal: exifDictionary?[kCGImagePropertyExifSubsecTimeOrginal as String] as? Int,
+            whiteBalance: exifDictionary?[kCGImagePropertyExifWhiteBalance as String] as? Int
         )
 
         if #available(iOS 13.1, *) {
-            exif.compositeImage = dictionary[kCGImagePropertyExifCompositeImage as String] as? Int
+            exif.compositeImage = exifDictionary?[kCGImagePropertyExifCompositeImage as String] as? Int
         }
 
         if #available(iOS 13, *) {
-            exif.offsetTime = dictionary[kCGImagePropertyExifOffsetTime as String] as? String
-            exif.offsetTimeDigitized = dictionary[kCGImagePropertyExifOffsetTimeDigitized as String] as? String
-            exif.offsetTimeOriginal = dictionary[kCGImagePropertyExifOffsetTimeOriginal as String] as? String
+            exif.offsetTime = exifDictionary?[kCGImagePropertyExifOffsetTime as String] as? String
+            exif.offsetTimeDigitized = exifDictionary?[kCGImagePropertyExifOffsetTimeDigitized as String] as? String
+            exif.offsetTimeOriginal = exifDictionary?[kCGImagePropertyExifOffsetTimeOriginal as String] as? String
         }
 
+        let gpsDictionary = dictionary["{GPS}"] as? [String: Any]
         let gps = GPS(
-            altitude: dictionary[kCGImagePropertyGPSAltitude as String] as? String,
-            altitudeRef: dictionary[kCGImagePropertyGPSAltitudeRef as String] as? Int,
-            destBearing: dictionary[kCGImagePropertyGPSDestBearing as String] as? String,
-            destBearingRef: dictionary[kCGImagePropertyGPSDestBearingRef as String] as? String,
-            hPositioningError: dictionary[kCGImagePropertyGPSHPositioningError as String] as? Int,
-            imgDirection: dictionary[kCGImagePropertyGPSImgDirection as String] as? String,
-            imgDirectionRef: dictionary[kCGImagePropertyGPSImgDirectionRef as String] as? String,
-            latitude: dictionary[kCGImagePropertyGPSLatitude as String] as? String,
-            latitudeRef: dictionary[kCGImagePropertyGPSLatitudeRef as String] as? String,
-            longitude: dictionary[kCGImagePropertyGPSLongitude as String] as? String,
-            longitudeRef: dictionary[kCGImagePropertyGPSLongitudeRef as String] as? String,
-            speed: dictionary[kCGImagePropertyGPSSpeed as String] as? Int,
-            speedRef: dictionary[kCGImagePropertyGPSSpeedRef as String] as? String
+            altitude: gpsDictionary?[kCGImagePropertyGPSAltitude as String] as? String,
+            altitudeRef: gpsDictionary?[kCGImagePropertyGPSAltitudeRef as String] as? Int,
+            destBearing: gpsDictionary?[kCGImagePropertyGPSDestBearing as String] as? String,
+            destBearingRef: gpsDictionary?[kCGImagePropertyGPSDestBearingRef as String] as? String,
+            hPositioningError: gpsDictionary?[kCGImagePropertyGPSHPositioningError as String] as? Int,
+            imgDirection: gpsDictionary?[kCGImagePropertyGPSImgDirection as String] as? String,
+            imgDirectionRef: gpsDictionary?[kCGImagePropertyGPSImgDirectionRef as String] as? String,
+            latitude: gpsDictionary?[kCGImagePropertyGPSLatitude as String] as? String,
+            latitudeRef: gpsDictionary?[kCGImagePropertyGPSLatitudeRef as String] as? String,
+            longitude: gpsDictionary?[kCGImagePropertyGPSLongitude as String] as? String,
+            longitudeRef: gpsDictionary?[kCGImagePropertyGPSLongitudeRef as String] as? String,
+            speed: gpsDictionary?[kCGImagePropertyGPSSpeed as String] as? Int,
+            speedRef: gpsDictionary?[kCGImagePropertyGPSSpeedRef as String] as? String
         )
 
+        let tiffDictionary = dictionary["{TIFF}"] as? [String: Any]
         let tiff = TIFF(
-            dateTime: dictionary[kCGImagePropertyTIFFDateTime as String] as? String,
-            make: dictionary[kCGImagePropertyTIFFMake as String] as? String,
-            model: dictionary[kCGImagePropertyTIFFModel as String] as? String,
-            orientation: dictionary[kCGImagePropertyTIFFOrientation as String] as? Int,
-            resolutionUnit: dictionary[kCGImagePropertyTIFFResolutionUnit as String] as? Int,
-            software: dictionary[kCGImagePropertyTIFFSoftware as String] as? String,
-            tileLength: dictionary[kCGImagePropertyTIFFTileLength as String] as? Int,
-            tileWidth: dictionary[kCGImagePropertyTIFFTileWidth as String] as? Int,
-            xResolution: dictionary[kCGImagePropertyTIFFXResolution as String] as? Int,
-            yResolution: dictionary[kCGImagePropertyTIFFYResolution as String] as? Int
+            dateTime: tiffDictionary?[kCGImagePropertyTIFFDateTime as String] as? String,
+            make: tiffDictionary?[kCGImagePropertyTIFFMake as String] as? String,
+            model: tiffDictionary?[kCGImagePropertyTIFFModel as String] as? String,
+            orientation: tiffDictionary?[kCGImagePropertyTIFFOrientation as String] as? Int,
+            resolutionUnit: tiffDictionary?[kCGImagePropertyTIFFResolutionUnit as String] as? Int,
+            software: tiffDictionary?[kCGImagePropertyTIFFSoftware as String] as? String,
+            tileLength: tiffDictionary?[kCGImagePropertyTIFFTileLength as String] as? Int,
+            tileWidth: tiffDictionary?[kCGImagePropertyTIFFTileWidth as String] as? Int,
+            xResolution: tiffDictionary?[kCGImagePropertyTIFFXResolution as String] as? Int,
+            yResolution: tiffDictionary?[kCGImagePropertyTIFFYResolution as String] as? Int
         )
 
         var properties = Self(
