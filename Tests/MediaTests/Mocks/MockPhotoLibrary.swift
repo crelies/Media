1d0ed82dc4ee9a793f81247841c845bac5172909
Christian Elies <chris.elies13@gmail.com>
2021-01-06 18:59:56 +0100

    test(): fixed tests

--- Tests/MediaTests/Mocks/MockPhotoLibrary.swift
+++ Tests/MediaTests/Mocks/MockPhotoLibrary.swift
@@ -27,6 +27,11 @@ final class MockPhotoLibrary: PhotoLibrary {
         handler(authorizationStatusToReturn)
     }
 
+    @available(iOS 14, macOS 11, macCatalyst 14, tvOS 14, *)
+    static func requestAuthorization(for accessLevel: PHAccessLevel, handler: @escaping RequestAuthorizationHandler) {
+        handler(authorizationStatusToReturn)
+    }
+
     func performChanges(_ changeBlock: @escaping () -> Void, completionHandler: ((Bool, Error?) -> Void)? = nil) {
         completionHandler?(Self.performChangesSuccess, Self.performChangesError)
         observer.map { $0.photoLibraryDidChange(PHChange()) }
