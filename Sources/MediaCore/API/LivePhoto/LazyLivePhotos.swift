fa7544dbefb3372f98a970e63650fd8e04f7904e
Christian Elies <chris.elies13@gmail.com>
2021-04-20 15:22:43 +0200

    added missing LazyLivePhotos.all

--- Sources/MediaCore/API/LivePhoto/LazyLivePhotos.swift
+++ Sources/MediaCore/API/LivePhoto/LazyLivePhotos.swift
@@ -26,3 +26,22 @@ public final class LazyLivePhotos {
         return .init(phAsset: asset)
     }
 }
+
+public extension LazyLivePhotos {
+    /// All live photos in the photo library
+    /// sorted by `creationDate descending` provided in a lazy container.
+    static var all: LazyLivePhotos? {
+        let mediaTypePredicate: NSPredicate = NSPredicate(format: "mediaType = %d", LivePhoto.type.rawValue)
+        let mediaSubtypePredicate: NSPredicate = Media.Filter<LivePhoto.MediaSubtype>.mediaSubtypes([.live]).predicate
+        let defaultSort: Media.Sort<Media.SortKey> = Media.Sort(key: .creationDate, ascending: false)
+        let options = PHFetchOptions()
+        options.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [mediaSubtypePredicate, mediaTypePredicate])
+        options.sortDescriptors = [defaultSort.sortDescriptor]
+        options.fetchLimit = 0
+        if let result = try? PHAssetFetcher.fetchAssets(options: options) {
+            return .init(result: result)
+        } else {
+            return nil
+        }
+    }
+}
