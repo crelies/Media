61647fca9d2e2a6d229f5ffc6e04716ad358d96a
Christian Elies <chris.elies13@gmail.com>
2021-05-01 01:08:53 +0200

    chore(example): added none lazy video examples

--- Example/Media-Example/Views/VideosSection.swift
+++ Example/Media-Example/Views/VideosSection.swift
@@ -12,28 +12,48 @@ import SwiftUI
 struct VideosSection: View {
     var body: some View {
         Section {
-            if let allVideos = LazyVideos.all {
-                NavigationLink(destination: LazyVideosView(videos: allVideos)) {
-                    Text("Videos.all (\(allVideos.count))")
+            if let lazyAllVideos = LazyVideos.all {
+                NavigationLink(destination: LazyVideosView(videos: lazyAllVideos)) {
+                    Text("LazyVideos.all (\(lazyAllVideos.count))")
                 }
             }
 
-            if let highFrameRatesVideos = LazyVideos.highFrameRates {
-                NavigationLink(destination: LazyVideosView(videos: highFrameRatesVideos)) {
-                    Text("Videos.highFrameRates (\(highFrameRatesVideos.count))")
+            let allVideos = Videos.all
+            NavigationLink(destination: VideosView(videos: allVideos)) {
+                Text("Videos.all (\(allVideos.count))")
+            }
+
+            if let lazyHighFrameRatesVideos = LazyVideos.highFrameRates {
+                NavigationLink(destination: LazyVideosView(videos: lazyHighFrameRatesVideos)) {
+                    Text("LazyVideos.highFrameRates (\(lazyHighFrameRatesVideos.count))")
                 }
             }
 
-            if let streamsVideos = LazyVideos.streams {
-                NavigationLink(destination: LazyVideosView(videos: streamsVideos)) {
-                    Text("Videos.streams (\(streamsVideos.count))")
+            let highFrameRatesVideos = Videos.highFrameRates
+            NavigationLink(destination: VideosView(videos: highFrameRatesVideos)) {
+                Text("Videos.highFrameRates (\(highFrameRatesVideos.count))")
+            }
+
+            if let lazyStreamsVideos = LazyVideos.streams {
+                NavigationLink(destination: LazyVideosView(videos: lazyStreamsVideos)) {
+                    Text("LazyVideos.streams (\(lazyStreamsVideos.count))")
                 }
             }
 
-            if let timelapsesVideos = LazyVideos.timelapses {
-                NavigationLink(destination: LazyVideosView(videos: timelapsesVideos)) {
-                    Text("Videos.timelapses (\(timelapsesVideos.count))")
+            let streamsVideos = Videos.streams
+            NavigationLink(destination: VideosView(videos: streamsVideos)) {
+                Text("Videos.streams (\(streamsVideos.count))")
+            }
+
+            if let lazyTimelapsesVideos = LazyVideos.timelapses {
+                NavigationLink(destination: LazyVideosView(videos: lazyTimelapsesVideos)) {
+                    Text("LazyVideos.timelapses (\(lazyTimelapsesVideos.count))")
                 }
+            }
+
+            let timelapsesVideos = Videos.timelapses
+            NavigationLink(destination: VideosView(videos: timelapsesVideos)) {
+                Text("Videos.timelapses (\(timelapsesVideos.count))")
             }
         }
     }
