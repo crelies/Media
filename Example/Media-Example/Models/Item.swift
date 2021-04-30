f69cf3e4f619ddc0dd8a5bb0334f81e97741c784
Christian Elies <chris.elies13@gmail.com>
2021-05-01 00:10:16 +0200

    chore(example): fixed bugs in lazy tree item implementation

--- Example/Media-Example/Models/Item.swift
+++ Example/Media-Example/Models/Item.swift
@@ -10,18 +10,37 @@ import MediaCore
 import SwiftUI
 
 enum Item: Identifiable {
-    var id: Self { self }
+    var id: String {
+        switch self {
+        case let .albums(albums):
+            return albums.id
+        case let .album(albums, index):
+            return "\(albums.id)\(index)"
+        case let .livePhotos(album):
+            return album?.id ?? UUID().uuidString
+        case let.livePhoto(livePhotos, index):
+            return "\(livePhotos.id)\(index)"
+        case let .photos(album):
+            return album?.id ?? UUID().uuidString
+        case let .photo(photos, index):
+            return "\(photos.id)\(index)"
+        case let .videos(album):
+            return album?.id ?? UUID().uuidString
+        case let .video(videos, index):
+            return "\(videos.id)\(index)"
+        }
+    }
 
     case albums(albums: LazyAlbums)
     case album(albums: LazyAlbums, index: Int)
 
-    case livePhotos(albumProvider: () -> LazyAlbum?)
+    case livePhotos(album: LazyAlbum?)
     case livePhoto(livePhotos: LazyLivePhotos, index: Int)
 
-    case photos(albumProvider: () -> LazyAlbum?)
+    case photos(album: LazyAlbum?)
     case photo(photos: Media.LazyPhotos, index: Int)
 
-    case videos(albumProvider: () -> LazyAlbum?)
+    case videos(album: LazyAlbum?)
     case video(videos: LazyVideos, index: Int)
 }
 
@@ -32,47 +51,20 @@ extension Item: Equatable {
             return leftAlbums == rightAlbums
         case (.album(let leftAlbums, let leftIndex), album(let rightAlbums, let rightIndex)):
             return leftAlbums == rightAlbums && leftIndex == rightIndex
-        case (.livePhotos(let leftAlbumProvider), livePhotos(let rightAlbumProvider)):
-            return leftAlbumProvider() == rightAlbumProvider()
+        case (.livePhotos(let leftAlbum), livePhotos(let rightAlbum)):
+            return leftAlbum == rightAlbum
         case (.livePhoto(let leftLivePhotos, let leftIndex), livePhoto(let rightLivePhotos, let rightIndex)):
             return leftLivePhotos == rightLivePhotos && leftIndex == rightIndex
-        case (.photos(let leftAlbumProvider), photos(let rightAlbumProvider)):
-            return leftAlbumProvider() == rightAlbumProvider()
+        case (.photos(let leftAlbum), photos(let rightAlbum)):
+            return leftAlbum == rightAlbum
         case (.photo(let leftPhotos, let leftIndex), photo(let rightPhotos, let rightIndex)):
             return leftPhotos == rightPhotos && leftIndex == rightIndex
-        case (.videos(let leftAlbumProvider), videos(let rightAlbumProvider)):
-            return leftAlbumProvider() == rightAlbumProvider()
+        case (.videos(let leftAlbum), videos(let rightAlbum)):
+            return leftAlbum == rightAlbum
         case (.video(let leftVideos, let leftIndex), video(let rightVideos, let rightIndex)):
             return leftVideos == rightVideos && leftIndex == rightIndex
         default:
             return false
-        }
-    }
-}
-
-extension Item: Hashable {
-    func hash(into hasher: inout Hasher) {
-        switch self {
-        case let .albums(albums):
-            hasher.combine(albums)
-        case let .album(albums, index):
-            hasher.combine(albums)
-            hasher.combine(index)
-        case let .livePhotos(albumProvider):
-            hasher.combine(albumProvider())
-        case let .livePhoto(livePhotos, index):
-            hasher.combine(livePhotos)
-            hasher.combine(index)
-        case let .photos(albumProvider):
-            hasher.combine(albumProvider())
-        case let .photo(items, index):
-            hasher.combine(items)
-            hasher.combine(index)
-        case let .videos(albumProvider):
-            hasher.combine(albumProvider())
-        case let .video(videos, index):
-            hasher.combine(videos)
-            hasher.combine(index)
         }
     }
 }
@@ -84,24 +76,24 @@ extension Item: Node {
             return (0..<albums.count).map { Item.album(albums: albums, index: $0) }
         case let .album(albums, index):
             return [
-                .livePhotos(albumProvider: { albums[index] }),
-                .photos(albumProvider: { albums[index] }),
-                .videos(albumProvider: { albums[index] })
+                .livePhotos(album: albums[index]),
+                .photos(album: albums[index]),
+                .videos(album: albums[index])
             ]
-        case let .livePhotos(albumProvider):
-            if let album = albumProvider(), let livePhotos = album.livePhotos, livePhotos.count > 0 {
+        case let .livePhotos(album):
+            if let album = album, let livePhotos = album.livePhotos, livePhotos.count > 0 {
                 return (0..<livePhotos.count).map { Item.livePhoto(livePhotos: livePhotos, index: $0) }
             } else {
                 return nil
             }
-        case let .photos(albumProvider):
-            if let album = albumProvider(), let photos = album.photos, photos.count > 0 {
+        case let .photos(album):
+            if let album = album, let photos = album.photos, photos.count > 0 {
                 return (0..<photos.count).map { Item.photo(photos: photos, index: $0) }
             } else {
                 return nil
             }
-        case let .videos(albumProvider):
-            if let album = albumProvider(), let videos = album.videos, videos.count > 0 {
+        case let .videos(album):
+            if let album = album, let videos = album.videos, videos.count > 0 {
                 return (0..<videos.count).map { Item.video(videos: videos, index: $0) }
             } else {
                 return nil
@@ -117,15 +109,10 @@ extension Item: Node {
             Text("Albums") + Text(" (\(albums.count))").font(.footnote)
         case let .album(albums, index):
             LazyView(data: { albums[index] }) { album in
-                if let album = album {
-                    let mediaCount = [album.audios?.count, album.livePhotos?.count, album.photos?.count, album.videos?.count].compactMap { $0 }.reduce(into: 0) { result, item in
-                        result = result + item
-                    }
-                    Text(album.id) + Text("\n") + Text("\(mediaCount) media items").font(.footnote)
-                }
+                Text("Album at index \(index)")
             }
-        case let .livePhotos(albumProvider):
-            if let album = albumProvider(), let livePhotos = album.livePhotos, livePhotos.count > 0 {
+        case let .livePhotos(album):
+            if let album = album, let livePhotos = album.livePhotos, livePhotos.count > 0 {
                 Text("Live Photos") + Text(" (\(livePhotos.count))").font(.footnote)
             }
         case let .livePhoto(livePhotos, index):
@@ -136,8 +123,8 @@ extension Item: Node {
                     }
                 }
             }
-        case let .photos(albumProvider):
-            if let album = albumProvider(), let photos = album.photos, photos.count > 0 {
+        case let .photos(album):
+            if let album = album, let photos = album.photos, photos.count > 0 {
                 Text("Photos") + Text(" (\(photos.count))").font(.footnote)
             }
         case let .photo(photos, index):
@@ -148,8 +135,8 @@ extension Item: Node {
                     }
                 }
             }
-        case let .videos(albumProvider):
-            if let album = albumProvider(), let videos = album.videos, videos.count > 0 {
+        case let .videos(album):
+            if let album = album, let videos = album.videos, videos.count > 0 {
                 Text("Videos") + Text(" (\(videos.count))").font(.footnote)
             }
         case let .video(videos, index):
