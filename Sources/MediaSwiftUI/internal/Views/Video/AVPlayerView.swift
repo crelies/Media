a67312ea8ac5a8680875cea7c3cb3d64f07fd6b8
Christian Elies <chris.elies13@gmail.com>
2021-04-30 23:46:20 +0200

    refactor(av player view): small improvements

--- Sources/MediaSwiftUI/internal/Views/Video/AVPlayerView.swift
+++ Sources/MediaSwiftUI/internal/Views/Video/AVPlayerView.swift
@@ -11,21 +11,23 @@ import SwiftUI
 
 @available(iOS 13, macOS 10.15, tvOS 13, *)
 struct AVPlayerView: UIViewControllerRepresentable {
-    private let wrapper = WeakObjectWrapper<AVPlayerViewController>()
+    private let wrapper = WeakObjectWrapper<AVPlayer>()
     let avPlayerItem: AVPlayerItem
 
     func makeUIViewController(context: UIViewControllerRepresentableContext<AVPlayerView>) -> AVPlayerViewController {
         let viewController = AVPlayerViewController()
         let player = AVPlayer(playerItem: avPlayerItem)
         viewController.player = player
-        wrapper.value = viewController
+        wrapper.value = player
         return viewController
     }
 
-    func updateUIViewController(_ uiViewController: AVPlayerViewController, context: UIViewControllerRepresentableContext<AVPlayerView>) {}
+    func updateUIViewController(_ uiViewController: AVPlayerViewController, context: UIViewControllerRepresentableContext<AVPlayerView>) {
+        uiViewController.player?.pause()
+    }
 
     func pause() {
-        wrapper.value?.player?.pause()
+        wrapper.value?.pause()
     }
 }
 #endif
