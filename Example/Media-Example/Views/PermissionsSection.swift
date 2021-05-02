efc42c12a1759d4ab8ae9eb5f2c5f18a2c5b1de5
Christian Elies <chris.elies13@gmail.com>
2021-05-02 12:42:48 +0200

    chore(example): improved permissions section

--- Example/Media-Example/Views/PermissionsSection.swift
+++ Example/Media-Example/Views/PermissionsSection.swift
@@ -8,23 +8,26 @@
 
 import AVFoundation
 import MediaCore
+import Photos
 import SwiftUI
 
 struct PermissionsSection: View {
     @State private var isLimitedLibraryPickerPresented = false
+    @State private var cameraPermission: AVAuthorizationStatus = .notDetermined
+    @State private var mediaPermission: PHAuthorizationStatus = .notDetermined
 
     var requestedPermission: (Result<Void, PermissionError>) -> Void
-    
+
     var body: some View {
         Section(header: Text("Permissions")) {
             Button(action: {
-                Media.requestCameraPermission { result in
-                    debugPrint(result)
+                Media.requestCameraPermission { _ in
+                    cameraPermission = Media.currentCameraPermission
                 }
             }) {
                 HStack {
                     Text("Trigger camera permission request")
-                    Toggle("", isOn: .constant(Media.currentCameraPermission == .authorized))
+                    Toggle("", isOn: .constant(cameraPermission == .authorized))
                         .disabled(true)
                 }
             }
@@ -38,17 +41,24 @@ struct PermissionsSection: View {
             }) {
                 HStack {
                     Text("Trigger photo library permission request")
-                    Toggle("", isOn: .constant(Media.currentPermission == .authorized))
+                    Toggle("", isOn: .constant(mediaPermission == .authorized))
                         .disabled(true)
                 }
             }
             .background(PHPicker(isPresented: $isLimitedLibraryPickerPresented))
+        }
+        .onAppear {
+            cameraPermission = Media.currentCameraPermission
+            mediaPermission = Media.currentPermission
         }
     }
 }
 
 private extension PermissionsSection {
     func requestPermission() {
-        Media.requestPermission(requestedPermission)
+        Media.requestPermission { result in
+            mediaPermission = Media.currentPermission
+            requestedPermission(result)
+        }
     }
 }
