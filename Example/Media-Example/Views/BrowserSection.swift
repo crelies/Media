ab15da702a8aa7ce255342fcd36859669186ca3f
Christian Elies <chris.elies13@gmail.com>
2021-05-03 13:14:30 +0200

    chore(example): added selected video in browser example

--- Example/Media-Example/Views/BrowserSection.swift
+++ Example/Media-Example/Views/BrowserSection.swift
@@ -6,15 +6,21 @@
 //  Copyright © 2021 Christian Elies. All rights reserved.
 //
 
+import AVKit
 import MediaCore
 import MediaSwiftUI
 import SwiftUI
+
+extension URL: Identifiable {
+    public var id: String { absoluteString }
+}
 
 struct BrowserSection: View {
     @State private var isLivePhotoBrowserViewVisible = false
     @State private var isMediaBrowserViewVisible = false
     @State private var isPhotoBrowserViewVisible = false
     @State private var isVideoBrowserViewVisible = false
+    @State private var playerURL: URL?
 
     var body: some View {
         Section(header: Label("Browser", systemImage: "photo.on.rectangle.angled")) {
@@ -59,8 +65,34 @@ struct BrowserSection: View {
             .fullScreenCover(isPresented: $isVideoBrowserViewVisible, onDismiss: {
                 isVideoBrowserViewVisible = false
             }) {
-                Video.browser(selectionLimit: 0) { _ in }
+                Video.browser(selectionLimit: 0, handleVideoBrowserResult)
             }
+            .background(
+                EmptyView()
+                    .sheet(item: $playerURL, onDismiss: {
+                        playerURL = nil
+                    }) { url in
+                        VideoPlayer(player: .init(url: url))
+                    }
+            )
+        }
+    }
+}
+
+private extension BrowserSection {
+    func handleVideoBrowserResult(_ result: Result<[BrowserResult<Video, URL>], Swift.Error>) {
+        switch result {
+        case let .success(browserResult):
+            switch browserResult.first {
+            case let .data(url):
+                isVideoBrowserViewVisible = false
+                // TODO: improve this
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                    playerURL = url
+                }
+            default: ()
+            }
+        default: ()
         }
     }
 }
