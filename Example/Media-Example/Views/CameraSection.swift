69d8b31591a6d37a873bfdab4a7bd3928b5c26e2
crelies <crelies@icloud.com>
2023-02-19 21:05:25 +0100

    chore(example): fixed camera section appearance

--- Example/Media-Example/Views/CameraSection.swift
+++ Example/Media-Example/Views/CameraSection.swift
@@ -32,116 +32,114 @@ struct CameraSection: View {
 
     var body: some View {
         Section(header: Label("Camera", systemImage: "camera")) {
-            VStack {
-                // TODO: [macOS] reuse existing custom photo camera view
+            // TODO: [macOS] reuse existing custom photo camera view
+            #if !os(macOS)
+            Button(action: {
+                isCameraViewVisible = true
+            }) {
+                Text("Camera.view")
+            }
+            .fullScreenCover(isPresented: $isCameraViewVisible, onDismiss: {
+                isCameraViewVisible = false
+            }) {
+                Camera.view(selection: $capturedMedia.onChange({ cameraResult in
+                    switch cameraResult {
+                    case let .tookPhoto(image: image):
+                        self.image = image
+                    case let .tookVideo(url):
+                        self.playerURL = url
+                    default: ()
+                    }
+                }), catchedError: .init(get: { nil }, set: { error in
+                    guard let error = error else {
+                        return
+                    }
+                    self.catchedError = .init(error: error)
+                }))
+            }
+            #endif
+
+            #if !targetEnvironment(macCatalyst) && !os(macOS)
+            Button(action: {
+                isLivePhotoCameraViewVisible = true
+            }) {
+                Text("LivePhoto.camera")
+            }
+            .fullScreenCover(isPresented: $isLivePhotoCameraViewVisible, onDismiss: {
+                isLivePhotoCameraViewVisible = false
+            }) {
+                LivePhoto.camera(cameraViewModel: cameraViewModel)
+            }
+            #endif
+
+            Button(action: {
+                isPhotoCameraViewVisible = true
+            }) {
+                Text("Photo.camera")
+            }
+            .universalFullScreenCover(isPresented: $isPhotoCameraViewVisible, onDismiss: {
+                isPhotoCameraViewVisible = false
+            }) {
                 #if !os(macOS)
-                Button(action: {
-                    isCameraViewVisible = true
-                }) {
-                    Text("Camera.view")
-                }
-                .fullScreenCover(isPresented: $isCameraViewVisible, onDismiss: {
-                    isCameraViewVisible = false
-                }) {
-                    Camera.view(selection: $capturedMedia.onChange({ cameraResult in
-                        switch cameraResult {
-                        case let .tookPhoto(image: image):
-                            self.image = image
-                        case let .tookVideo(url):
-                            self.playerURL = url
-                        default: ()
-                        }
-                    }), catchedError: .init(get: { nil }, set: { error in
-                        guard let error = error else {
-                            return
-                        }
-                        self.catchedError = .init(error: error)
-                    }))
-                }
-                #endif
-
-                #if !targetEnvironment(macCatalyst) && !os(macOS)
-                Button(action: {
-                    isLivePhotoCameraViewVisible = true
-                }) {
-                    Text("LivePhoto.camera")
-                }
-                .fullScreenCover(isPresented: $isLivePhotoCameraViewVisible, onDismiss: {
-                    isLivePhotoCameraViewVisible = false
-                }) {
-                    LivePhoto.camera(cameraViewModel: cameraViewModel)
-                }
-                #endif
-
-                Button(action: {
-                    isPhotoCameraViewVisible = true
-                }) {
-                    Text("Photo.camera")
-                }
-                .universalFullScreenCover(isPresented: $isPhotoCameraViewVisible, onDismiss: {
-                    isPhotoCameraViewVisible = false
-                }) {
-                    #if !os(macOS)
-                    Photo.camera(selection: $capturedPhoto.onChange({ cameraResult in
-                        switch cameraResult {
-                        case let .tookPhoto(image: image):
-                            self.image = image
-                        default: ()
-                        }
-                    }), catchedError: .init(get: { nil }, set: { error in
-                        guard let error = error else {
-                            return
-                        }
-                        self.catchedError = .init(error: error)
-                    }))
-                    #else
-                    Photo.camera(viewModel: cameraViewModel)
-                        .frame(minWidth: 400, minHeight: 400)
-                    #endif
-                }
-                .alert(item: $catchedError) {
-                    Alert(
-                        title: Text("Error"),
-                        message: Text($0.error.localizedDescription)
-                    )
-                }
-                .sheet(item: $image, onDismiss: {
-                    image = nil
-                }) { uiImage in
-                    Image(universalImage: uiImage)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                }
-
-                // TODO: [macOS] implement video camera (reuse existing custom photo camera view)
-                #if !os(macOS)
-                Button(action: {
-                    isVideoCameraViewVisible = true
-                }) {
-                    Text("Video.camera")
-                }
-                .fullScreenCover(isPresented: $isVideoCameraViewVisible, onDismiss: {
-                    isVideoCameraViewVisible = false
-                }) {
-                    Video.camera(selection: $recordedVideoURL.onChange({ result in
-                        guard let result = result else {
-                            return
-                        }
-                        self.playerURL = result.value
-                    }), catchedError: .init(get: { nil }, set: { error in
-                        guard let error = error else {
-                            return
-                        }
-                        self.catchedError = .init(error: error)
-                    }))
-                }
-                .sheet(item: $playerURL, onDismiss: {
-                    playerURL = nil
-                }) { url in
-                    VideoPlayer(player: .init(url: url))
-                }
+                Photo.camera(selection: $capturedPhoto.onChange({ cameraResult in
+                    switch cameraResult {
+                    case let .tookPhoto(image: image):
+                        self.image = image
+                    default: ()
+                    }
+                }), catchedError: .init(get: { nil }, set: { error in
+                    guard let error = error else {
+                        return
+                    }
+                    self.catchedError = .init(error: error)
+                }))
+                #else
+                Photo.camera(viewModel: cameraViewModel)
+                    .frame(minWidth: 400, minHeight: 400)
                 #endif
             }
+            .alert(item: $catchedError) {
+                Alert(
+                    title: Text("Error"),
+                    message: Text($0.error.localizedDescription)
+                )
+            }
+            .sheet(item: $image, onDismiss: {
+                image = nil
+            }) { uiImage in
+                Image(universalImage: uiImage)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }
+
+            // TODO: [macOS] implement video camera (reuse existing custom photo camera view)
+            #if !os(macOS)
+            Button(action: {
+                isVideoCameraViewVisible = true
+            }) {
+                Text("Video.camera")
+            }
+            .fullScreenCover(isPresented: $isVideoCameraViewVisible, onDismiss: {
+                isVideoCameraViewVisible = false
+            }) {
+                Video.camera(selection: $recordedVideoURL.onChange({ result in
+                    guard let result = result else {
+                        return
+                    }
+                    self.playerURL = result.value
+                }), catchedError: .init(get: { nil }, set: { error in
+                    guard let error = error else {
+                        return
+                    }
+                    self.catchedError = .init(error: error)
+                }))
+            }
+            .sheet(item: $playerURL, onDismiss: {
+                playerURL = nil
+            }) { url in
+                VideoPlayer(player: .init(url: url))
+            }
+            #endif
         }
     }
 }
