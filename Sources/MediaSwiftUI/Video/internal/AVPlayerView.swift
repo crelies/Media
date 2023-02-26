61a2761d9c558b28555f3a505941b8c809e2eaa8
celies <celies@eos-uptrade.de>
2023-02-26 14:29:29 +0100

    added todo

--- Sources/MediaSwiftUI/Video/internal/AVPlayerView.swift
+++ Sources/MediaSwiftUI/Video/internal/AVPlayerView.swift
@@ -9,26 +9,27 @@
 import AVKit
 import SwiftUI
 
-@available(iOS 13, macOS 10.15, tvOS 13, *)
-struct AVPlayerView: UIViewControllerRepresentable {
-    private let wrapper = WeakObjectWrapper<AVPlayer>()
-
-    let avPlayerItem: AVPlayerItem
-
-    func makeUIViewController(context: UIViewControllerRepresentableContext<AVPlayerView>) -> AVPlayerViewController {
-        let viewController = AVPlayerViewController()
-        let player = AVPlayer(playerItem: avPlayerItem)
-        viewController.player = player
-        wrapper.value = player
-        return viewController
-    }
-
-    func updateUIViewController(_ uiViewController: AVPlayerViewController, context: UIViewControllerRepresentableContext<AVPlayerView>) {
-        uiViewController.player?.pause()
-    }
-
-    func pause() {
-        wrapper.value?.pause()
-    }
-}
+// TODO: unused?
+//@available(iOS 13, macOS 10.15, tvOS 13, *)
+//struct AVPlayerView: UIViewControllerRepresentable {
+//    private let wrapper = WeakObjectWrapper<AVPlayer>()
+//
+//    let avPlayerItem: AVPlayerItem
+//
+//    func makeUIViewController(context: UIViewControllerRepresentableContext<AVPlayerView>) -> AVPlayerViewController {
+//        let viewController = AVPlayerViewController()
+//        let player = AVPlayer(playerItem: avPlayerItem)
+//        viewController.player = player
+//        wrapper.value = player
+//        return viewController
+//    }
+//
+//    func updateUIViewController(_ uiViewController: AVPlayerViewController, context: UIViewControllerRepresentableContext<AVPlayerView>) {
+//        uiViewController.player?.pause()
+//    }
+//
+//    func pause() {
+//        wrapper.value?.pause()
+//    }
+//}
 #endif
